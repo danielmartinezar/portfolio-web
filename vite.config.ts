b825961498e0ea7aa211d14d035586e651ca06e6
Daniel Martinez Arias <73305373+danielmartinezar@users.noreply.github.com>
2026-02-04 16:43:53 -0500

    :sparkles: FEAT: Add local markdown implementation

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 import vike from "vike/plugin";
+import { vikeVercelPlugin } from "@vite-plugin-vercel/vike";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(), vike()],
+  plugins: [react(), tailwindcss(), svgr(), vike(), vikeVercelPlugin()],
   server: {
     allowedHosts: ["9ce476db48b9.ngrok-free.app"],
   },
